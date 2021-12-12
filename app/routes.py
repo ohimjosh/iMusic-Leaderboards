cf18dacf82c56509ec1ed223c1643c36b10c3c20
Josh <joshmiranda.exe@gmail.com>
2021-12-12 18:31:08 -0500

    chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
 from app import app
+from flask import render_template
+from app.forms import LoginForm
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Josh'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body' : 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body' : 'The Avengers movie was so cool!'
+            
+        }
+    ]
+    
+    return render_template ('index.html', title = 'Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template ('login.html', title='Sign In', form=form)
